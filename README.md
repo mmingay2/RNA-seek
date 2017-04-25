32816fc771bc4886c1a985da2bc75da4cad4fc6c
mmingay2 <mmingay2@gmail.com>
2017-04-24 23:26:16 -0700

    Upload app

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # RNA-seek
-Explore RNA-seq gene expression patterns within MSigDB gene lists.
+
+####Explore RNA-seq gene expression patterns within MSigDB gene lists.
+
+#####[A beta version of this feature is available here](https://mmingay2.shinyapps.io/RNA-seek/)
+
+######Powered by [Shiny](shiny.rstudio.com) and [RStudio](https://www.rstudio.com/)
+
+
+
+
+
+
---
+++ RNA-seek/app.R
@@ -0,0 +1,89 @@
+library(shiny)
+library(shinydashboard)
+library(datasets)
+library(ggplot2)
+library(shinythemes)
+library(tidyr)
+library(reshape2)
+library(data.table)
+library(dplyr)
+
+##Set theme
+
+commonTheme = list(labs(color="Density",fill="Density"),
+                   theme_bw(),
+                   theme(legend.position=c(0,1),
+                         legend.justification=c(0,1)))
+
+##Load and process Data 
+
+rpks <- as.data.frame(fread("./rpks.txt"))
+allg <- readRDS(file = "./msigdesc.rds")
+msigdb <- readRDS("./msigdb2.rds")
+datab <- readRDS(file="./vitclists.RDS")
+
+##Create User Interface object which designs the webpage and allows for user input through input$"inputID"
+
+ui <- navbarPage(title="Vitamin C induced epigenetic remodelling", theme = shinythemes::shinytheme("sandstone"),
+                 tabPanel(title = "RNA-seek",
+                          fluidPage(
+                            
+                            # Sidebar with controls to select the variable to plot against mpg
+                            # and to specify whether outliers should be included
+                            sidebarPanel(
+                              selectInput("fillm", "Colour genes in msigDB list:", c(allg, names(datab)), multiple = F, selected = allg[3554])
+                              
+                            ),
+                            # Show the caption and plot of the requested variable against mpg
+                            mainPanel(
+                              h3(textOutput("log(1+RPKM) for mm10v71 genes")),
+                              plotOutput("rnaPlot", 
+                                         width = "100%",  height = "100%",
+                                         hover = "plot_hover",
+                                         click = "plot_click"), 
+                              dataTableOutput("infob")
+                            )))
+)
+
+#Create a server object that generates output$ plots or tables that are reactitve '({})' to changes in the UI (input$*) to update the output (output$*)
+
+server <- shinyServer(function(input, output) {
+  
+  plata <- reactive({ 
+    x <- log(1+rpks[,paste0("unt_1")])
+    y <- log(1+rpks[,paste0("vitc_1")])
+    z <- rpks$gene %in% c(msigdb[[input$fillm]], toupper(datab[[input$fillm]]$V1))
+    #z <- rpks$gene %in% toupper(data[[input$fillm]]$V1)
+    gene <- rpks$gene
+    ensid <- rpks$ensid
+    df <- data.frame(gene, ensid, x,y,z)
+    colnames(df) <- c("gene", "ensid", "Untreated", "VitaminC", "InGroup")
+    df
+  })
+  
+  
+  output$rnaPlot <- renderPlot({
+    plata() %>%
+      ggplot(aes(x=Untreated, y=VitaminC, colour=InGroup, size=InGroup, alpha=InGroup))+
+      # ggplot(data=rpks, aes(x=log(unt_2), y=log(vitc_2),colour=get(input$fillx))+
+      geom_point(position="jitter")+
+      #   geom_point(data=subset(rpks, input$fillx != "n.s"), aes(x=log(get(paste0("unt_",input$exp_num))), y=log(get(paste0("vitc_",input$exp_num))), colour=get(input$fillx)), position="jitter", size=1.2)+
+      theme_bw()+
+      xlab("Untreated")+
+      ylab("Vitamin C")+
+      scale_size_manual(values=c(0.5,1))+
+      scale_alpha_manual(values=c(0.25, 1))+
+      guides(alpha=FALSE, size=FALSE)+
+      scale_colour_manual(values=c("gray65", "red"), guide = guide_legend(title = "ingroup?"))+
+      geom_abline(slope=1, color="gray10")+
+      ggtitle("Click Points on Plot to view Gene ID and RPKM")
+    #coord_cartesian(xlim=c(-2,4), ylim=c(-2,4))
+  }, width = 700, height = 400)
+  
+  output$infob <- renderDataTable(
+    plata() %>% mutate(fc=log2(VitaminC/Untreated)) %>% nearPoints(input$plot_click, maxpoints = 25, threshold = 10))
+  
+})
+
+shinyApp(ui = ui, server = server)
+
